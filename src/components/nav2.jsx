ef07b22b3d490cdfba772c748677315096d3dd02
Dekiru <dwyl.agency1@gmail.com>
2026-07-02 14:42:34 +0530

    nav2.jsx

---
+++ src/components/nav2.jsx
@@ -0,0 +1,10 @@
+function Nav2(props) {
+    return <div className="div2">
+        <h4>HOME</h4>
+        <h4>CONTACT</h4>
+        <h4>ABOUT</h4>
+        <h4>{props.theme}</h4>
+        
+    </div>
+}
+export default Nav2
